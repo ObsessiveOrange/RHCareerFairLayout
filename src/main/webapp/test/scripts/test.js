8b63b2fac72e16f880af19b315ee3ab7697bdc3e
Benedict Wong <bennydictwong@gmail.com>
2015-04-05 10:10:26 -0400

    Added dynamic styling to list generation

--- src/main/webapp/test/scripts/test.js
+++ src/main/webapp/test/scripts/test.js
@@ -1,11 +1,9 @@
 var careerFairData;
 var companyList;
-
 $(document).ready(function() {
     var options = {
         valueNames: ['show', 'company', 'table', 'info']
     };
-    companyList = new List('CompanyList', options);
     getInitialRequest();
 });
 
@@ -18,23 +16,30 @@ function getInitialRequest() {
         url: "/api/data?method=getData",
         successHandler: function(data) {
             careerFairData = $.parseJSON(data);
-            //      $(".careerFairDescription").html(careerFairData.title);
             $("span.careerFairDescription").html(careerFairData.title);
             updateCompanyList();
+            companyList = new List('CompanyList', options);
         }
     });
 }
 
 function updateCompanyList() {
+    var companyListBody = $("#companyListBody");
     for (var key in careerFairData.entries) {
         if (careerFairData.entries.hasOwnProperty(key)) {
             var entry = careerFairData.entries[key];
-            companyList.add({
-                show: "<img src='images/checkboxChecked.png' class='checkbox'/>",
-                company: entry.title,
-                table: entry.parameters.table,
-                info: "{i}"
-            });
+            companyListBody.append("<tr>");
+            companyListBody.append("<td class='show center'  onclick='toggleCheckbox(" + entry.id + ")'><img src='images/checkboxChecked.png' class='checkbox' id='showOnMapCheckbox_" + entry.id + "/></td>");
+            companyListBody.append("<td class='company' onclick='toggleCheckbox(" + entry.id + ")'>" + entry.title + "</td>");
+            companyListBody.append("<td class='table center'>" + entry.parameters.table + "</td>");
+            companyListBody.append("<td class='info center'>[i]</td>");
+            companyListBody.append("</tr>");
+            // companyList.add({
+            //     show: "<img src='images/checkboxChecked.png' class='checkbox'/>",
+            //     company: entry.title,
+            //     table: entry.parameters.table,
+            //     info: "{i}"
+            // });
             entry.checked = true;
         }
     }
