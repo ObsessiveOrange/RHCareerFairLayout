69a1e3764ae9481e76689cea61a7591a0dd958d6
Benedict Wong <bennydictwong@gmail.com>
2015-04-07 00:54:58 -0400

    Corrected import, syntax

--- src/main/webapp/test/scripts/filters.js
+++ src/main/webapp/test/scripts/filters.js
@@ -49,228 +49,228 @@ function createFilterList() {
             }
         }
     }
+}
 
-    function showfilterGroupID(groupID) {
-        $("#showOnMapCheckbox_" + groupID).text("▲");
-        $(".filterGroup" + groupID + "Element").css("display", "table-row");
-    }
+function showfilterGroupID(groupID) {
+    $("#showOnMapCheckbox_" + groupID).text("▲");
+    $(".filterGroup" + groupID + "Element").css("display", "table-row");
+}
 
-    function hidefilterGroupID(groupID) {
-        $("#filtersListExpand_" + groupID).text("▼");
-        $(".filterGroup" + groupID + "Element").css("display", "hidden");
-        highlightedTables.removeFromOrderedList(careerFairData.entries[groupID].parameters.table);
-    }
+function hidefilterGroupID(groupID) {
+    $("#filtersListExpand_" + groupID).text("▼");
+    $(".filterGroup" + groupID + "Element").css("display", "hidden");
+    highlightedTables.removeFromOrderedList(careerFairData.entries[groupID].parameters.table);
+}
 
-    function togglefilterGroupID(groupID) {
-        if ($("#filtersListExpand_" + groupID).text() == "▼") {
-            showfilterGroupID(groupID);
-        } else {
-            hidefilterGroupID(groupID);
-        }
+function togglefilterGroupID(groupID) {
+    if ($("#filtersListExpand_" + groupID).text() == "▼") {
+        showfilterGroupID(groupID);
+    } else {
+        hidefilterGroupID(groupID);
     }
-    
-    //draw tables and table numbers
-    function drawRect(tableNumber, x, y, width, height) {
-        $mapCanvasTables.drawLine({
-            //    layer: true,
-            strokeStyle: '#000',
-            strokeWidth: scaling,
-            x1: x,
-            y1: y,
-            x2: x + width,
-            y2: y,
-            x3: x + width,
-            y3: y + height,
-            x4: x,
-            y4: y + height,
-            closed: true,
-            //    click : function(layer) {
-            //      alert("You clicked an area!");
-            //    } //Box and text both need to be a layer for this to work.
-        });
-        if (tableNumber != 0) {
-            $mapCanvasTables.drawText({
-                //      layer: true,
-                fillStyle: '#000000',
-                x: x + width / 2,
-                y: y + height / 2,
-                fontSize: height / 2,
-                fontFamily: 'Verdana, sans-serif',
-                text: tableNumber
-            });
-        }
-    }
-    //generate positions of all tables.
-    function generateTableLocations() {
-        tableLocations = [];
-        var hrzCount = careerFairData.layout.section2 + 2;
-        var vrtCount = Math.max(careerFairData.layout.section1, careerFairData.layout.section3);
-        unitX = $mapCanvasTables.prop("width") / 100;
-        tableWidth = unitX * 80 / hrzCount;
-        unitY = $mapCanvasTables.prop("width") / 2 / 100;
-        tableHeight = unitY * 70 / vrtCount;
-        var s1 = careerFairData.layout.section1;
-        var s2 = careerFairData.layout.section2;
-        var s2Rows = careerFairData.layout.section2Rows;
-        var s2PathWidth = careerFairData.layout.section2PathWidth;
-        var s3 = careerFairData.layout.section3;
-        // section 1
-        for (var i = 0; i < s1; i++) {
-            tableLocations.push({
-                x: 5 * unitX,
-                y: 5 * unitY + i * tableHeight
-            });
-        }
-        // section 2
-        var pathWidth = (unitY * 70 - s2Rows * tableHeight) / (s2Rows / 2);
-        //rows
-        for (var i = 0; i < s2Rows; i++) {
-            //outer rows have no walkway
-            if (i == 0 || i == s2Rows - 1) {
-                for (var j = 0; j < s2; j++) {
-                    tableLocations.push({
-                        x: (10 * unitX) + ((1 + j) * tableWidth),
-                        y: 5 * unitY + Math.floor((i + 1) / 2) * pathWidth + i * tableHeight
-                    });
-                }
-            }
-            //inner rows need to have walkway halfway through
-            else {
-                var leftTables = Math.floor((s2 - s2PathWidth) / 2);
-                var rightTables = s2 - 2 - leftTables;
-                for (var j = 0; j < leftTables; j++) {
-                    tableLocations.push({
-                        x: (10 * unitX) + ((1 + j) * tableWidth),
-                        y: 5 * unitY + Math.floor((i + 1) / 2) * pathWidth + i * tableHeight
-                    });
-                }
-                for (var j = 0; j < rightTables; j++) {
-                    tableLocations.push({
-                        x: (10 * unitX) + ((1 + leftTables + 2 + j) * tableWidth),
-                        y: 5 * unitY + Math.floor((i + 1) / 2) * pathWidth + i * tableHeight
-                    });
-                }
-            }
-        }
-        // section 3
-        for (var i = 0; i < s3; i++) {
-            tableLocations.push({
-                x: (15 * unitX) + ((1 + s2) * tableWidth),
-                y: 5 * unitY + i * tableHeight
-            });
-        }
-    }
-    //draw actual tables, then draw registration and rest areas
-    function drawTables($mapCanvasTables) {
-        for (var i = 0; i < tableLocations.length; i++) {
-            var locationX = tableLocations[i].x;
-            var locationY = tableLocations[i].y;
-            drawRect(i + 1, locationX, locationY, tableWidth, tableHeight);
-        }
-        // rest & registration areas
-        drawRect(0, 40 * unitX, 80 * unitY, 45 * unitX, 15 * unitY);
+}
+//draw tables and table numbers
+function drawRect(tableNumber, x, y, width, height) {
+    $mapCanvasTables.drawLine({
+        //    layer: true,
+        strokeStyle: '#000',
+        strokeWidth: scaling,
+        x1: x,
+        y1: y,
+        x2: x + width,
+        y2: y,
+        x3: x + width,
+        y3: y + height,
+        x4: x,
+        y4: y + height,
+        closed: true,
+        //    click : function(layer) {
+        //      alert("You clicked an area!");
+        //    } //Box and text both need to be a layer for this to work.
+    });
+    if (tableNumber != 0) {
         $mapCanvasTables.drawText({
-            //    layer: true,
+            //      layer: true,
             fillStyle: '#000000',
-            x: 62.5 * unitX,
-            y: 87.5 * unitY,
-            fontSize: 20 * scaling,
+            x: x + width / 2,
+            y: y + height / 2,
+            fontSize: height / 2,
             fontFamily: 'Verdana, sans-serif',
-            text: 'Rest Area'
-        });
-        drawRect(0, 5 * unitX, 80 * unitY, 30 * unitX, 15 * unitY);
-        $mapCanvasTables.drawText({
-            //    layer: true,
-            fillStyle: '#000000',
-            x: 20 * unitX,
-            y: 87.5 * unitY,
-            fontSize: 20 * scaling,
-            fontFamily: 'Verdana, sans-serif',
-            text: 'Registration'
+            text: tableNumber
         });
     }
-    //Highlight tables in array
-    function highlightTables(color) {
-        $mapCanvasHighlights.clearCanvas();
-        highlightedTables.forEach(function(table) {
-            highlightTable(table, color);
+}
+//generate positions of all tables.
+function generateTableLocations() {
+    tableLocations = [];
+    var hrzCount = careerFairData.layout.section2 + 2;
+    var vrtCount = Math.max(careerFairData.layout.section1, careerFairData.layout.section3);
+    unitX = $mapCanvasTables.prop("width") / 100;
+    tableWidth = unitX * 80 / hrzCount;
+    unitY = $mapCanvasTables.prop("width") / 2 / 100;
+    tableHeight = unitY * 70 / vrtCount;
+    var s1 = careerFairData.layout.section1;
+    var s2 = careerFairData.layout.section2;
+    var s2Rows = careerFairData.layout.section2Rows;
+    var s2PathWidth = careerFairData.layout.section2PathWidth;
+    var s3 = careerFairData.layout.section3;
+    // section 1
+    for (var i = 0; i < s1; i++) {
+        tableLocations.push({
+            x: 5 * unitX,
+            y: 5 * unitY + i * tableHeight
         });
     }
-
-    function highlightTable(id, color) {
-        var x = tableLocations[id - 1].x;
-        var y = tableLocations[id - 1].y;
-        $mapCanvasHighlights.drawRect({
-            fillStyle: color,
-            x: x,
-            y: y,
-            width: tableWidth,
-            height: tableHeight,
-            fromCenter: false
-        });
-    }
-
-    function sendGetRequest(requestObject) {
-        $.ajax({
-            url: requestObject.url,
-            type: "GET",
-            headers: requestObject.headers,
-            data: requestObject.data,
-            success: requestObject.successHandler
-        });
-    };
-
-    function sendPostRequest(requestObject) {
-        $.ajax({
-            url: requestObject.url,
-            type: "POST",
-            headers: requestObject.headers,
-            data: requestObject.data,
-            success: requestObject.successHandler
-        });
-    }
-    Array.prototype.insertAtIndex = function(item, index) {
-        this.splice(index, 0, item);
-    };
-    Array.prototype.addToOrderedList = function(item) {
-        var insertIndex;
-        for (insertIndex = 0; insertIndex < this.length; insertIndex++) {
-            if (item < this[insertIndex]) {
-                break;
+    // section 2
+    var pathWidth = (unitY * 70 - s2Rows * tableHeight) / (s2Rows / 2);
+    //rows
+    for (var i = 0; i < s2Rows; i++) {
+        //outer rows have no walkway
+        if (i == 0 || i == s2Rows - 1) {
+            for (var j = 0; j < s2; j++) {
+                tableLocations.push({
+                    x: (10 * unitX) + ((1 + j) * tableWidth),
+                    y: 5 * unitY + Math.floor((i + 1) / 2) * pathWidth + i * tableHeight
+                });
             }
         }
-        this.insertAtIndex(item, insertIndex);
-    }
-    Array.prototype.addArrayToOrderedList = function(array) {
-        this.push.
-        this.sort();
-    }
-    Array.prototype.findInOrderedList = function(item) {
-        var low = 0;
-        var high = this.length - 1;
-        var mid = Math.floor(this.length / 2);
-        if (this.length == 0) {
-            return -1;
-        }
-        while (low >= 0 && high < this.length && low <= high) {
-            if (item == this[mid]) {
-                return mid;
-            } else if (low == high) {
-                return -1;
-            } else if (item < this[mid]) {
-                high = mid - 1;
-                mid = Math.floor((low + high) / 2);
-            } else {
-                low = mid + 1
-                mid = Math.floor((low + high) / 2);
+        //inner rows need to have walkway halfway through
+        else {
+            var leftTables = Math.floor((s2 - s2PathWidth) / 2);
+            var rightTables = s2 - 2 - leftTables;
+            for (var j = 0; j < leftTables; j++) {
+                tableLocations.push({
+                    x: (10 * unitX) + ((1 + j) * tableWidth),
+                    y: 5 * unitY + Math.floor((i + 1) / 2) * pathWidth + i * tableHeight
+                });
+            }
+            for (var j = 0; j < rightTables; j++) {
+                tableLocations.push({
+                    x: (10 * unitX) + ((1 + leftTables + 2 + j) * tableWidth),
+                    y: 5 * unitY + Math.floor((i + 1) / 2) * pathWidth + i * tableHeight
+                });
             }
         }
+    }
+    // section 3
+    for (var i = 0; i < s3; i++) {
+        tableLocations.push({
+            x: (15 * unitX) + ((1 + s2) * tableWidth),
+            y: 5 * unitY + i * tableHeight
+        });
+    }
+}
+//draw actual tables, then draw registration and rest areas
+function drawTables($mapCanvasTables) {
+    for (var i = 0; i < tableLocations.length; i++) {
+        var locationX = tableLocations[i].x;
+        var locationY = tableLocations[i].y;
+        drawRect(i + 1, locationX, locationY, tableWidth, tableHeight);
+    }
+    // rest & registration areas
+    drawRect(0, 40 * unitX, 80 * unitY, 45 * unitX, 15 * unitY);
+    $mapCanvasTables.drawText({
+        //    layer: true,
+        fillStyle: '#000000',
+        x: 62.5 * unitX,
+        y: 87.5 * unitY,
+        fontSize: 20 * scaling,
+        fontFamily: 'Verdana, sans-serif',
+        text: 'Rest Area'
+    });
+    drawRect(0, 5 * unitX, 80 * unitY, 30 * unitX, 15 * unitY);
+    $mapCanvasTables.drawText({
+        //    layer: true,
+        fillStyle: '#000000',
+        x: 20 * unitX,
+        y: 87.5 * unitY,
+        fontSize: 20 * scaling,
+        fontFamily: 'Verdana, sans-serif',
+        text: 'Registration'
+    });
+}
+//Highlight tables in array
+function highlightTables(color) {
+    $mapCanvasHighlights.clearCanvas();
+    highlightedTables.forEach(function(table) {
+        highlightTable(table, color);
+    });
+}
+
+function highlightTable(id, color) {
+    var x = tableLocations[id - 1].x;
+    var y = tableLocations[id - 1].y;
+    $mapCanvasHighlights.drawRect({
+        fillStyle: color,
+        x: x,
+        y: y,
+        width: tableWidth,
+        height: tableHeight,
+        fromCenter: false
+    });
+}
+
+function sendGetRequest(requestObject) {
+    $.ajax({
+        url: requestObject.url,
+        type: "GET",
+        headers: requestObject.headers,
+        data: requestObject.data,
+        success: requestObject.successHandler
+    });
+};
+
+function sendPostRequest(requestObject) {
+    $.ajax({
+        url: requestObject.url,
+        type: "POST",
+        headers: requestObject.headers,
+        data: requestObject.data,
+        success: requestObject.successHandler
+    });
+}
+Array.prototype.insertAtIndex = function(item, index) {
+    this.splice(index, 0, item);
+};
+Array.prototype.addToOrderedList = function(item) {
+    var insertIndex;
+    for (insertIndex = 0; insertIndex < this.length; insertIndex++) {
+        if (item < this[insertIndex]) {
+            break;
+        }
+    }
+    this.insertAtIndex(item, insertIndex);
+}
+Array.prototype.addArrayToOrderedList = function(array) {
+    this.push.
+    this.sort();
+}
+Array.prototype.findInOrderedList = function(item) {
+    var low = 0;
+    var high = this.length - 1;
+    var mid = Math.floor(this.length / 2);
+    if (this.length == 0) {
         return -1;
     }
-    Array.prototype.removeFromOrderedList = function(item) {
-        var index = this.findInOrderedList(item);
-        if (index > -1) {
-            return this.splice(index, 1);
+    while (low >= 0 && high < this.length && low <= high) {
+        if (item == this[mid]) {
+            return mid;
+        } else if (low == high) {
+            return -1;
+        } else if (item < this[mid]) {
+            high = mid - 1;
+            mid = Math.floor((low + high) / 2);
+        } else {
+            low = mid + 1
+            mid = Math.floor((low + high) / 2);
         }
-        return [];
     }
+    return -1;
+}
+Array.prototype.removeFromOrderedList = function(item) {
+    var index = this.findInOrderedList(item);
+    if (index > -1) {
+        return this.splice(index, 1);
+    }
+    return [];
+}
