8f3d7db7c5be737932e8a7deed7a029b87638f47
Benedict Wong <bennydictwong@gmail.com>
2015-04-06 23:35:37 -0400

    Added type flag

--- src/main/java/adt/Category.java
+++ src/main/java/adt/Category.java
@@ -11,18 +11,15 @@ public class Category extends Item implements Comparable<Category> {
      */
     private static final long  serialVersionUID = -7441122606014580042L;
     private String             title;
+    private String             type;
     private ArrayList<Integer> entries;
-    
-    public Category() {
-    
-        super(0);
-    }
     
     public Category(String title, String type) {
     
         super(DataServlet.dataVars.getNextCategoryID());
         
         this.title = title;
+        this.type = type;
         this.entries = new ArrayList<Integer>();
         
     }
