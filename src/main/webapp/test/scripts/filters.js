dae20b03ee8b0e3780fa21792ec0851cbd12918c
Benedict Wong <bennydictwong@gmail.com>
2015-04-07 16:38:51 -0400

    Forced filterIDs to be numbers for consistency

--- src/main/webapp/test/scripts/filters.js
+++ src/main/webapp/test/scripts/filters.js
@@ -60,6 +60,7 @@ function createFilterList() {
         var filterGroupID = types.length;
         $filtersListBody.append("<tr class='filtersListGroupRow' id='filtersListGroup" + filterGroupID + "Row' onclick='toggleFilterGroupID(" + filterGroupID + ")'><td class='center filtersListExpandColumn' id='filtersListExpand_" + filterGroupID + "'>▼</td><td class='filtersListFilterColumn'><b>" + filterGroup + "</b></td>");
         Object.keys(careerFairData.categories[filterGroup]).forEach(function(filterID) {
+            filterID = Number(filterID);
             $filtersListBody.append("<tr class='filterGroup" + filterGroupID + "Element'><td class='center filtersListSelectColumn' onclick='toggleCheckbox(" + '"' + filterGroup + '", ' + filterID + ")' id='selectFilterCheckbox_" + filterID + "'>☐</td><td class='filtersListFilterColumn' onclick='toggleCheckbox(" + '"' + filterGroup + '", ' + filterID + ")'>" + careerFairData.categories[filterGroup][filterID].title + "</td></tr>");
             if(filters[filterGroup].indexOf(filterID) != -1){
                 markCheckboxChecked(filterGroup, filterID);
@@ -95,13 +96,13 @@ function toggleFilterGroupID(groupID) {
 
 function markCheckboxChecked(groupName, filterID) {
     $("#selectFilterCheckbox_" + filterID).text("☑");
-    filters[groupName].addToOrderedSet(filterID.toString());
+    filters[groupName].addToOrderedSet(filterID);
     filters.changed = true;
 }
 
 function markCheckboxUnchecked(groupName, filterID) {
     $("#selectFilterCheckbox_" + filterID).text("☐");
-    filters[groupName].removeFromOrderedSet(filterID.toString());
+    filters[groupName].removeFromOrderedSet(filterID);
     filters.changed = true;
 }
 
