b3a988a17ed732cd7019535a835e40b1b907827a
Benedict Wong <bennydictwong@gmail.com>
2015-04-08 00:45:30 -0400

    Added unload handler to sessVars test page

--- src/main/webapp/test/scripts/test.js
+++ src/main/webapp/test/scripts/test.js
@@ -2,7 +2,13 @@ $(document).ready(function(){
     alert(SessionVars.retreiveString("testKey"));
 
     SessionVars.clear();
-    alert("storing testKey:testValue");
-    SessionVars.storeString("testKey", "testValue");
+
 
 });
+
+window.onbeforeunload = function(event) {
+    alert("Unloading");
+    alert("storing testKey:testValue");
+    SessionVars.storeString("testKey", "testValue");
+    
+}
