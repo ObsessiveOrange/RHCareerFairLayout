563d8e6865d63878d8b3957231f156da04493711
Benedict Wong <bennydictwong@gmail.com>
2015-04-09 20:13:39 -0400

    Switched back to 1/8th circle for arrow

--- src/main/webapp/test/scripts/index.js
+++ src/main/webapp/test/scripts/index.js
@@ -319,9 +319,8 @@ function drawTutorial() {
     $canvas.drawText({
         fillStyle: '#00F',
         x: $("#filterBtn").offset().left + $("#filterBtn").width() / 2 - 200 - $canvas.measureText('Click it to filter the companies').width / 2,
-        y: $("#filterBtn").offset().top + $("#filterBtn").height() / 2 + 250,
+        y: $("#filterBtn").offset().top + $("#filterBtn").height() / 2 + 50 + 200/Math.sqrt(2),
         maxWidth: $canvas.measureText('Click it to filter the companies').width / 2,
-        fontSize: height / 2,
         fontFamily: 'Verdana, sans-serif',
         text: "This is the filter button. Click it to filter the companies"
     });
