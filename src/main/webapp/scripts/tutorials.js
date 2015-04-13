97e14050d2d8bb76f8594bc114968fbaf05f991a
Benedict Wong <bennydictwong@gmail.com>
2015-04-13 19:44:05 -0400

    Added call to unbind resize function

--- src/main/webapp/scripts/tutorials.js
+++ src/main/webapp/scripts/tutorials.js
@@ -79,6 +79,7 @@ function endTutorial() {
         skipButton.remove();
         skipButton = null;
     }
+    $window.unbind('resize');
     $canvas.remove();
     unlockScrolling();
     showScrollbars();
