b45e1e232119e5dee3e23d661d685e79da9aba6d
Benedict Wong <bennydictwong@gmail.com>
2015-04-21 14:37:16 -0400

    Added new google analytics tracking information

--- src/main/webapp/scripts/gAnalytics.js
+++ src/main/webapp/scripts/gAnalytics.js
@@ -9,5 +9,5 @@
     a.src = g;
     m.parentNode.insertBefore(a, m)
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-ga('create', 'UA-61976205-1', 'auto');
+ga('create', 'UA-61976205-2', 'auto');
 ga('send', 'pageview');
