fc26e4f178894f85689ebe8fc1a0c5436d72eef1
Benedict Wong <bennydictwong@gmail.com>
2015-04-26 18:16:45 -0400

    Removed test method - getParameter not fully supported on servlet 3.0

--- src/main/java/servlets/admin/AdminServlet.java
+++ src/main/java/servlets/admin/AdminServlet.java
@@ -123,7 +123,6 @@ public class AdminServlet extends HttpServlet {
         // }
         
         response.setContentType("application/json");
-        String test = request.getParameter("method");
         
         Response respObj = new SuccessResponse("File Upload successful");
         boolean isMultipart = ServletFileUpload.isMultipartContent(request);
