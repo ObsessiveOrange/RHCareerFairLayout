8bc3f76a7efdb5afcf03f056fcdcb408d4eb32fc
Benedict Wong <bennydictwong@gmail.com>
2015-04-26 01:14:25 -0400

    try a different way

--- src/main/webapp/scripts/test.js
+++ src/main/webapp/scripts/test.js
@@ -2,36 +2,60 @@ var globalData;
 $(document).ready(function() {
     $("#uploadForm").submit(function(e) {
         e.preventDefault();
-        var formData = new FormData();
-        $.each($('#file')[0].files, function(i, file) {
-            formData.append('file-' + i, file);
-        });
+        // var formData = new FormData();
+        // $.each($('#file')[0].files, function(i, file) {
+        //     formData.append('file-' + i, file);
+        // });
+        // $.ajax({
+        //     url: '/api/users/admin?method=test', //Server script to process data
+        //     type: 'POST',
+        //     xhr: function() { // Custom XMLHttpRequest
+        //         var myXhr = $.ajaxSettings.xhr();
+        //         if (myXhr.upload) { // Check if upload property exists
+        //             myXhr.upload.addEventListener('progress', progressHandlingFunction, false); // For handling the progress of the upload
+        //         }
+        //         return myXhr;
+        //     },
+        //     //Ajax events
+        //     // beforeSend: beforeSendHandler,
+        //     success: function(data) {
+        //         globalData = data;
+        //         console.log(data);
+        //     },
+        //     headers: {
+        //         "authUser": "bennydictwong",
+        //     },
+        //     // error: errorHandler,
+        //     // Form data
+        //     data: formData,
+        //     //Options to tell jQuery not to process data or worry about content-type.
+        //     cache: false,
+        //     contentType: false,
+        //     processData: false
+        // });
+        var form = $("#uploadForm");
+        var formdata = false;
+        if (window.FormData) {
+            formdata = new FormData(form[0]);
+        }
+        var formAction = form.attr('action');
         $.ajax({
-            url: '/api/users/admin?method=test', //Server script to process data
+            url: '/api/users/admin?method=test',
+            data: formdata ? formdata : form.serialize(),
+            cache: false,
+            contentType: false,
+            processData: false,
             type: 'POST',
-            xhr: function() { // Custom XMLHttpRequest
-                var myXhr = $.ajaxSettings.xhr();
-                if (myXhr.upload) { // Check if upload property exists
-                    myXhr.upload.addEventListener('progress', progressHandlingFunction, false); // For handling the progress of the upload
-                }
-                return myXhr;
-            },
-            //Ajax events
-            // beforeSend: beforeSendHandler,
-            success: function(data) {
+            success: function(data, textStatus, jqXHR) {
+                // Callback code
                 globalData = data;
                 console.log(data);
             },
-            headers: {
-                "authUser": "bennydictwong",
+            error: function(jqXHR, textStatus, errorThrown ) {
+                console.log("failed");
+                console.log(textStatus);
+                console.log(errorThrown);
             },
-            // error: errorHandler,
-            // Form data
-            data: formData,
-            //Options to tell jQuery not to process data or worry about content-type.
-            cache: false,
-            contentType: false,
-            processData: false
         });
     });
 });
