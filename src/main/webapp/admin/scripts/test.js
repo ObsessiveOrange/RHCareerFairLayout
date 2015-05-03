7380ed4843aacac54bb2876bdff643085fe28d3b
Benedict Wong <bennydictwong@gmail.com>
2015-05-03 01:58:45 -0400

    Removed call to ready()

--- src/main/webapp/admin/scripts/test.js
+++ src/main/webapp/admin/scripts/test.js
@@ -1,31 +1,29 @@
-$(document).ready(function() {
-    for (i = 0; i < 5; i++) {
-        $("#yearField").append("<option value='" + (new Date().getFullYear() + i) + "'>" + (new Date().getFullYear() + i) + "</option>");
-    }
-    $("#submit").click(function() {
-        $.ajax({
-            url: "/api/users/admin?method=uploadData",
-            type: "POST",
-            contentType: false,
-            processData: false,
-            data: function() {
-                var data = new FormData();
-                data.append("chosenFile", $("#file").get(0).files[0]);
-                return data;
-                // Or simply return new FormData($("form")[0]);
-            }(),
-            error: function(_, textStatus, errorThrown) {
-                alert("Error");
-                console.log(textStatus, errorThrown);
-            },
-            success: function(response, textStatus) {
-                alert("Success");
-                console.log(response, textStatus);
-            },
-            headers: {
-                "year": $("#yearField").val(),
-                "term": $("#termField").val()
-            }
-        });
+for (i = 0; i < 5; i++) {
+    $("#yearField").append("<option value='" + (new Date().getFullYear() + i) + "'>" + (new Date().getFullYear() + i) + "</option>");
+}
+$("#submit").click(function() {
+    $.ajax({
+        url: "/api/users/admin?method=uploadData",
+        type: "POST",
+        contentType: false,
+        processData: false,
+        data: function() {
+            var data = new FormData();
+            data.append("chosenFile", $("#file").get(0).files[0]);
+            return data;
+            // Or simply return new FormData($("form")[0]);
+        }(),
+        error: function(_, textStatus, errorThrown) {
+            alert("Error");
+            console.log(textStatus, errorThrown);
+        },
+        success: function(response, textStatus) {
+            alert("Success");
+            console.log(response, textStatus);
+        },
+        headers: {
+            "year": $("#yearField").val(),
+            "term": $("#termField").val()
+        }
     });
 });
