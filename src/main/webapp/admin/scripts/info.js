47910d236a9807dc28fca3fe12ea21e1d3a06e4b
Benedict Wong <bennydictwong@gmail.com>
2015-05-05 02:33:01 -0400

    changed sourceURL to absolute path

--- src/main/webapp/admin/scripts/info.js
+++ src/main/webapp/admin/scripts/info.js
@@ -1,44 +1,18 @@
-$(document).ready(function() {
-    sendGetRequest({
-        url: "/api/data?method=getActiveTerm",
-        successHandler: function(returnData) {
-            //
-            //set last fetch time, so we know to refresh beyond a certain validity time
-            if (returnData.success !== 0) {
-                $("#activeQuarter").html(returnData.activeQuarter);
-                $("#activeYear").html(returnData.activeYear);
-            } else {
-                alert("Error: Could not retreive data");
-            }
-        },
-        errorHandler: function(jqXHR, textStatus, errorThrown) {
-            console.log(textStatus + " : " + errorThrown);
-            $("#contentFrame").load("login.html");
+sendGetRequest({
+    url: "/api/data?method=getActiveTerm",
+    successHandler: function(returnData) {
+        //
+        //set last fetch time, so we know to refresh beyond a certain validity time
+        if (returnData.success !== 0) {
+            $("#activeQuarter").html(returnData.activeQuarter);
+            $("#activeYear").html(returnData.activeYear);
+        } else {
+            alert("Error: Could not retreive data");
         }
-    });
+    },
+    errorHandler: function(jqXHR, textStatus, errorThrown) {
+        console.log(textStatus + " : " + errorThrown);
+        $("#contentFrame").load("login.html");
+    }
 });
-//
-//send get request
-function sendGetRequest(requestObject) {
-    $.ajax({
-        url: requestObject.url,
-        type: "GET",
-        headers: requestObject.headers,
-        data: requestObject.data,
-        success: requestObject.successHandler,
-        error: requestObject.errorHandler
-    });
-}
-//
-//send post request
-function sendPostRequest(requestObject) {
-    $.ajax({
-        url: requestObject.url,
-        type: "POST",
-        headers: requestObject.headers,
-        data: requestObject.data,
-        success: requestObject.successHandler,
-        error: requestObject.errorHandler
-    });
-}
-//# sourceURL=info.js
+//# sourceURL=/admin/scripts/info.js
