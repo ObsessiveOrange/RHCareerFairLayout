d26f4e70eabf9d9c99b7cd1a00f08edb0cc3e467
Benedict Wong <bennydictwong@gmail.com>
2015-05-10 14:02:37 -0400

    Added basic logic for companies admin view

--- src/main/webapp/admin/scripts/companies.js
+++ src/main/webapp/admin/scripts/companies.js
@@ -1,32 +1,71 @@
 (window.setup = function() {
     sendGetRequest({
-        url: "/api/data?method=getSelectedTerm",
-        successHandler: function(returnData) {
-            //
-            //set last fetch time, so we know to refresh beyond a certain validity time
-            if (returnData.success === 1) {
-                $("#selectedQuarter").html(returnData.selectedQuarter);
-                $("#selectedYear").html(returnData.selectedYear);
-            } else {
-                alert("Error: Could not retreive data");
-            }
-        },
-        errorHandler: function(jqXHR, textStatus, errorThrown) {
-            console.log(textStatus + " : " + errorThrown);
-            $("#contentFrame").load("login.html");
-        }
-    });
-    sendGetRequest({
-        url: "/api/data?method=getStatistics",
-        successHandler: function(returnData) {
-            //
-            //set last fetch time, so we know to refresh beyond a certain validity time
-            if (returnData.success === 1) {
-                $("#companyCount").html(returnData.companyCount);
-                $("#layoutTableCount").html(returnData.layoutTableCount);
-                $("#mappedTableCount").html(returnData.mappedTableCount);
-            } else {
-                alert("Error: Could not retreive data");
+        url: "/api/data?method=getCategories",
+        successHandler: function(companiesReturnData) {
+            if (companiesReturnData.success === 1) {
+                sendGetRequest({
+                    url: "/api/data?method=getCategories",
+                    successHandler: function(categoriesReturnData) {
+                        //
+                        //set last fetch time, so we know to refresh beyond a certain validity time
+                        if (categoriesReturnData.success === 1) {
+                            var companies = companiesReturnData.companies;
+                            var categories = categoriesReturnData.categories;
+                            var headersHtml = "<tr>";
+                            headersHtml += "<td>";
+                            headersHtml += "ID:";
+                            headersHtml += "</td>";
+                            headersHtml += "<td>";
+                            headersHtml += "Name:";
+                            headersHtml += "</td>";
+                            Object.keys(categories).forEach(function(categoryType) {
+                                headersHtml += "<td>";
+                                headersHtml += categoryType + "s:";
+                                headersHtml += "</td>";
+                            });
+                            headersHtml += "<td>";
+                            headersHtml += "tableNumber";
+                            headersHtml += "</td>";
+                            headersHtml += "</tr>";
+                            $("#companiesTable").html(headersHtml);
+                            Object.keys(companies).forEach(function(companyId) {
+                                var company = companies[companyId];
+                                var companyHtml = "<tr>";
+                                companyHtml += "<td>";
+                                companyHtml += company.id;
+                                companyHtml += "</td>";
+                                companyHtml += "<td>";
+                                companyHtml += company.name;
+                                companyHtml += "</td>";
+                                Object.keys(categories).forEach(function(categoryType) {
+                                    var first = true;
+                                    companyHtml += "<td>";
+                                    _.intersection(Object.keys(categories[categoryType]), company.categories).forEach(function(categoryId) {
+                                        var category = categories[categoryType][categoryId];
+                                        if (!first) {
+                                            companyHtml += ", ";
+                                        }
+                                        companyHtml += category.name;
+                                        first = true;
+                                    });
+                                    companyHtml += "</td>";
+                                });
+                                companyHtml += "<td>";
+                                companyHtml += company.tableNumber;
+                                companyHtml += "</td>";
+                                companyHtml += "</tr>";
+                                $("#companiesTable").append(companyHtml);
+                                $("#companiesTable").append("<tr><td><br /><br /></td></tr>");
+                            });
+                        } else {
+                            alert("Error: Could not retreive data");
+                        }
+                    },
+                    errorHandler: function(jqXHR, textStatus, errorThrown) {
+                        console.log(textStatus + " : " + errorThrown);
+                        $("#contentFrame").load("login.html");
+                    }
+                });
             }
         },
         errorHandler: function(jqXHR, textStatus, errorThrown) {
