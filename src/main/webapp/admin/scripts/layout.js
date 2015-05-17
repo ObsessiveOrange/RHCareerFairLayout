d0a36dbdf74c79ab79dee5d9ac14f1a959257e7e
Benedict Wong <bennydictwong@gmail.com>
2015-05-17 11:43:10 -0400

    Do not label if greater than locationTableMapping array length

--- src/main/webapp/admin/scripts/layout.js
+++ src/main/webapp/admin/scripts/layout.js
@@ -277,7 +277,7 @@ function drawTables() {
     //draw company tables based on generated locations
     Object.keys(tableLocations).forEach(function(key) {
         var location = tableLocations[key];
-        var tableNumber = (location.locationID >= tableLocations.length ? 0 : location.locationID);
+        var tableNumber = (location.locationID >= careerFairData.termVars.layout.locationTableMapping.length ? 0 : location.locationID);
         drawRect(tableNumber, location.x, location.y, location.width, location.height);
     });
     //
