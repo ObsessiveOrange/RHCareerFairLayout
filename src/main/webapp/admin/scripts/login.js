0163c69d2ec84758800afcf1d95b1c131cdc4a5f
Benedict Wong <bennydictwong@gmail.com>
2015-05-23 23:50:30 -0400

    Added headers

--- src/main/webapp/admin/scripts/login.js
+++ src/main/webapp/admin/scripts/login.js
@@ -1,22 +1,21 @@
 (window.setup = function() {
     $("#pageContents").on("click", "#login", function() {
         sendPostRequest({
-            url: "/api/data?method=getStatistics",
+            url: "/api/data?method=login",
+            headers: {},
             successHandler: function(returnData) {
                 //
                 //set last fetch time, so we know to refresh beyond a certain validity time
-            if (returnData.success === 1) {
-                loadContentWithJS("overview");
-                setupLinks();
-            } else {
-                loadContentWithJS("login");
-            }
-            },
-            errorHandler: function(jqXHR, textStatus, errorThrown) {
-                console.log(textStatus + " : " + errorThrown);
-                $("#contentFrame").load("login.html");
+                if (returnData.success === 1) {
+                    loadContentWithJS("overview");
+                    setupLinks();
+                } else {
+                    loadContentWithJS("login");
+                }
             }
         });
     });
 })();
-window.cleanup = function() {};
+window.cleanup = function() {
+    $("#pageContents").unbind();
+};
